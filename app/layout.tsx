2359f045c202d9d6ea10c7911289a31b858b6f8d
Mohamed Gamal <mohamed.g.shoaib@gmail.com>
2025-09-02 22:48:49 +0300

    feat: update layout to use data-theme attribute

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,7 +77,7 @@ export default async function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body>
         <ThemeProvider
-          attribute="class"
+          attribute="data-theme"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
